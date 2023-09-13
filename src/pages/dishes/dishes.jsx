bf6d48b668c1412880ff54e976494c528aea854d
Lakshya yadav <itslakshyayadav@gmail.com>
2023-09-13 23:09:49 +0530

    add fillter options

--- src/pages/dishes/dishes.jsx
+++ src/pages/dishes/dishes.jsx
@@ -32,11 +32,45 @@ const Dishes = () => {
 
   return (
     <div className="container mx-auto max-w-4xl lg:max-w-6xl my-6 px-4 md:px-6">
+      <hr className="mb-4" />
+      <div className="flex items-center gap-5">
+        <h1 className="text-3xl text-gray-800 font-sans font-semibold">
+          Fillters
+        </h1>
+        <div className="flex items-center border px-3 ">
+          <label
+            htmlFor="breakfast"
+            className="font-semibold px-3 py-2 text-gray-600"
+          >
+            Breakfast
+          </label>
+          <input id="breakfast" type="checkbox" className="w-4 h-4 px-3 " />
+        </div>
+        <div className="flex items-center px-3 border ">
+          <label
+            htmlFor="dinner"
+            className="font-semibold px-3 py-2 text-gray-600"
+          >
+            Dinner
+          </label>
+          <input id="dinner" type="checkbox" className="w-4 h-4 px-3" />
+        </div>
+        <div className="flex items-center px-3 border">
+          <label
+            htmlFor="lunch"
+            className="font-semibold px-3 py-2 text-gray-600"
+          >
+            Lunch
+          </label>
+          <input id="lunch" type="checkbox" className="w-4 h-4 px-3" />
+        </div>
+      </div>
+      <hr className="mt-4" />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-2 lg:gap-3">
         {dishes.map((dishItem, index) => {
           return (
             dishItem.isActive && (
-              <div key={"dish-card" + index}>
+              <div key={"dish-card" + index} className="hover:shadow-lg">
                 <div className="mt-6 border rounded-2xl">
                   <Link to={`/dishes/${dishItem._id}`}>
                     <img
